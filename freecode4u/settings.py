d42ea16058ff326f6d5fe97c7abb0b52f708d31e
Dennis <dennisndegwa001@gmail.com>
2025-01-04 08:45:00 +0300

    Add Cors

--- freecode4u/settings.py
+++ freecode4u/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'clients',
     'items',
     'payments',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
